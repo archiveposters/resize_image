58112756f815f26d9564d549ffb20ac6f29bb29f
jamiebird119 <jamie.bird119@gmail.com>
2021-01-27 17:16:00 +0000

    Sizes conversion from cm to pixels introduced and error handling of dpi info not available.

--- resize.py
+++ resize.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import csv
 import os
 from os import listdir
@@ -8,23 +8,29 @@ def resize_image(image, size):
     # Load image and resize using pillow - currently not maintaining aspect ratio
     img = Image.open(image)
     fname = image.replace("images/", "").replace(".jpg", "").replace(".png", "")
-    newsize = (size[0],size[1])
+    
+
+    # Convert size from cm to pixels
+    newsize = (int(size[0]*37.795275591),int(size[1]*37.795275591)) 
 
     # Image is resized below - Can be edited to client specification
     img_new = img.resize(newsize)
     size_string = f"{size[0]}x{size[1]}"
-    
+
+    # Set border at 5% of resized image
+    border = (int((size[0]/100)*5),int((size[1]/100)*5))
+    img_new_plusborder =  ImageOps.expand(img_new, border=border)
+
     # Uses image dpi however if info not available will return false
-    if 'dpi' in img.info.keys() :
+    if 'dpi' in img_new_plusborder.info.keys() :
         dpi = img_new.info['dpi'][0] * img_new.info['dpi'][1]
         if dpi >= 300:
-            img_new.save(f"./resized_images/{fname}-{size_string}.jpg","JPEG")
+            img_new_plusborder.save(f"./resized_images/{fname}-{size_string}.jpg","JPEG")
             return size_string
         else:
             return False
     else:
-        img_new.save(f"./resized_images/{fname}-{size_string}.jpg","JPEG")
-        return size_string
+        return "Does Not Contain DPI info"
 
 
 def resize_and_write():
@@ -35,7 +41,7 @@ def resize_and_write():
     # [[x1,y1],[[x2,y2]] etc
     #
 
-    sizes = [[10,20],[30,50]]
+    sizes = [[29.7,42],[21,29.7],[14.8,21],[30,40],[45,60]]
 
     # Will take each file in image_folder and run the resize saving only if dpi matches specification and adding to csv file 
     for f in image_folder:
@@ -47,7 +53,10 @@ def resize_and_write():
             for size in sizes:
                 img_resize_function = resize_image(file_path, size)
                 if img_resize_function:
-                    applied_sizes.append(img_resize_function)
+                    if img_resize_function == "Does Not Contain DPI info":
+                        applied_sizes = "Does Not Contain DPI info"
+                    else:    
+                        applied_sizes.append(img_resize_function)
             writer.writeheader()       
             writer.writerow({"filename": f, "sizes": applied_sizes})
 
